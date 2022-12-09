b10e37a55fd2ad54835d16b39be316b14f9e51b7
Uditha Atukorala <ua@nuked.zone>
2022-12-09 23:52:39 +0000

    (day-7) part 2

--- src/day-7/puzzle.cpp
+++ src/day-7/puzzle.cpp
@@ -1,8 +1,11 @@
 #include "puzzle.h"
 
+#include <map>
 #include <regex>
 #include <stack>
 #include <string>
+#include <utility>
+#include <vector>
 
 namespace aoc {
 unsigned long part_1(std::istream &input) {
@@ -59,5 +62,50 @@ unsigned long part_1(std::istream &input) {
 	}
 
 	return result;
+}
+
+unsigned long part_2(std::istream &input) {
+	const auto total = 70'000'000lu;
+	const auto want  = 30'000'000lu;
+
+	const std::regex re{R"(^(\d+).*$)"};
+	std::smatch      m;
+
+	std::map<std::string, unsigned long> dirs;
+	std::string                          path;
+
+	unsigned long ul = 0;
+	for (std::string line; std::getline(input, line);) {
+		if (line.starts_with("$ cd ")) {
+			if (line.substr(5, 2) == "..") {
+				path.erase(path.rfind("/"));
+			} else {
+				path += "/" + line.substr(5);
+			}
+		}
+
+		if (!std::regex_match(line, m, re)) {
+			continue;
+		}
+
+		ul = std::stoul(m[1]);
+		for (auto p = path; p.size() > 1; p = p.erase(p.rfind("/"))) {
+			dirs[p] += ul;
+		}
+	}
+
+	const auto min = want - (total - dirs["//"]);
+
+	// Sort dirs by size
+	std::vector<std::pair<std::string, unsigned long>> dirz{dirs.begin(), dirs.end()};
+	std::sort(dirz.begin(), dirz.end(), [](auto l, auto r) { return l.second < r.second; });
+
+	for (const auto &d : dirz) {
+		if (d.second >= min) {
+			return d.second;
+		}
+	}
+
+	return 0;
 }
 } // namespace aoc
--- src/day-7/puzzle.h
+++ src/day-7/puzzle.h
@@ -4,4 +4,5 @@
 
 namespace aoc {
 unsigned long part_1(std::istream &input);
+unsigned long part_2(std::istream &input);
 } // namespace aoc
